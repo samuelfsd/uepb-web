c4ddcec925061c9d7e7b459bbe8f24aa61cb1805
samuelfsd <samuel.estudo778@gmail.com>
2024-08-27 14:36:56 -0300

    feat: add aula 04

---
+++ aula-04/main.js
@@ -0,0 +1,27 @@
+// js here !!!
+
+// comentário de uma linha
+
+/* isto é um comentário longo
+   de múltiplas linhas.
+ */
+
+// /* Você não pode, porém, /* aninhar comentários */ SyntaxError */
+
+
+// declaração de variáveis
+
+// tipos de dados ->
+
+// operadores aritméticos -> +, -, *, /, %
+
+// operadores de comparação -> == === != > < >= <=
+
+// operadores lógicos -> && || ! -> !!
+
+// controle de fluxo -> if else else if
+
+// estruturas de repetições -> for forin forof
+
+// funções
+
