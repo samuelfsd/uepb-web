7a9be9598d661c1cecbb562af07d9e69a93e7ad9
samuelfsd <samuel.estudo778@gmail.com>
2024-09-30 22:04:03 -0300

    feat: add thunderclient

--- aula-10/src/server.js
+++ aula-10/src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-app.listen(3001, () => console.log('server running on port 3001'));
+app.listen(3333, () => console.log('server running on port 3333'));
 
 app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
